ccea674ee50b803e6a6f3e3becc9dce1818b3d02
Cebu Web Design <info@olivergonzales.com>
2016-07-10 23:07:05 +0900

    trans update
    for notify

---
+++ resources/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo '["http://i.froala.com/assets/photo1.jpg","http://i.froala.com/assets/photo2.jpg","http://i.froala.com/assets/photo3.jpg","http://i.froala.com/assets/photo4.jpg","http://i.froala.com/assets/photo5.jpg","http://i.froala.com/assets/photo6.jpg","http://i.froala.com/assets/photo7.jpg","http://i.froala.com/assets/photo8.jpg","http://i.froala.com/assets/photo9.jpg","http://i.froala.com/assets/photo10.jpg"]';
+
+?>
